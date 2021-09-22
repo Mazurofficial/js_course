d4c8902ccd8ea27f81de815a594390eb8d4956d6
Mazurofficial <vladmazurenkog@gmail.com>
2021-09-22 12:17:10 +0300

    app 1 task

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hello");
+
+const numberOfFilms = +prompt("How much films have you seen ?");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+const filmName = prompt('What film was last ?');
+const rating = prompt('How cool it was ?'); 
+personalMovieDB.movies[filmName] = rating;
+console.log(personalMovieDB);
